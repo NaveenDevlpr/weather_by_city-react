37a8df28947b0eb2fe79ccd0b0410c3eac51803f
naveen <naveenmk675@gmail.com>
2024-03-01 14:16:41 +0530

    project created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="max-w-7xl min-h-screen flex items-center justify-center">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/SearchBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBox = () => {
+  return (
+    <div className='w-full flex flex-row space-x-2'>
+      
+    </div>
+  )
+}
+
+export default SearchBox
